c590bcae8d09029ebc87a5af8185cdc6fc0337f0
Daniel Lee <daniellee0217@yahoo.com>
2022-11-19 19:07:30 -0500

    Added updating the Spiral-Stats repository for new data before each execution

--- Run.bat
+++ Run.bat
@@ -1,4 +1,9 @@
-@echo off
+@echo "STARTING SCRIPTS"
+@echo "Updating Spiral-Stats repository"
+for %%I in (.) do set CurrDirName=%%~nxI
+cd ../Spiral-Stats
+git pull origin main
+cd ../%CurrDirName%
 python ./genshin_teams_aggregation_from_gcsim.py
 python ./genshin_teams_aggregation_from_spiral_stats.py
 python ./genshin_teams_aggregation_from_mihoyo.py
