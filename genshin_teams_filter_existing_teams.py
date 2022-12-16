832b21cd6cf2e22e66c8d67a063acbae253bab43
Daniel Lee <daniellee0217@yahoo.com>
2022-12-16 02:44:00 -0500

    Updated to 3.3 abyss

--- genshin_teams_filter_existing_teams.py
+++ genshin_teams_filter_existing_teams.py
@@ -51,89 +51,90 @@ travelerReview = np.empty((0, 4), str)
 
 for filename in os.listdir(directory):
     processFile = os.path.join(directory, filename)
-    # Remove listings less than 4 rows
-    print("REMOVING TEAMS WITH LESS THAN 4 CHARACTERS")
+    if os.stat(processFile).st_size != 0:
+        # Remove listings less than 4 rows
+        print("REMOVING TEAMS WITH LESS THAN 4 CHARACTERS")
 
-    with open(processFile, 'r') as infile, open(processFile[:-4] + "Filtered" + ".csv", 'w') as outfile:
-        csv_reader = csv.reader(infile)
-        csv_writer = csv.writer(outfile)
-        for line in csv_reader:
-            if all(line):
-                csv_writer.writerow(line)
+        with open(processFile, 'r') as infile, open(processFile[:-4] + "Filtered" + ".csv", 'w') as outfile:
+            csv_reader = csv.reader(infile)
+            csv_writer = csv.writer(outfile)
+            for line in csv_reader:
+                if all(line):
+                    csv_writer.writerow(line)
 
-    pf = open(processFile[:-4] + "Filtered" + ".csv", 'r')
-    data = np.loadtxt(pf, delimiter=",", dtype=str)
+        pf = open(processFile[:-4] + "Filtered" + ".csv", 'r')
+        data = np.loadtxt(pf, delimiter=",", dtype=str)
 
-    # Convert to shorthand
-    print("CONVERTING CHARACTER NAMES TO COMPATIBLE FORMAT")
-    for charData in characterData:
-        if charData["fullName"] != "Traveler":
-            data[data == charData["fullName"]] = charData["shortName"]
-    data = np.sort(data)
+        # Convert to shorthand
+        print("CONVERTING CHARACTER NAMES TO COMPATIBLE FORMAT")
+        for charData in characterData:
+            if charData["fullName"] != "Traveler":
+                data[data == charData["fullName"]] = charData["shortName"]
+        data = np.sort(data)
 
-    # Removing duplicates
-    uniqueConvert = [tuple(row) for row in data]
-    data = np.unique(uniqueConvert, axis=0)
-    
-    # Remove entries that are already in main sheet and other sheets
-    data = setdiff2d_bc(data, mainFileArray)
-    data = setdiff2d_bc(data, mainData)
-    if len(denyListArray) > 0:
-        print("REMOVING DENY LIST ITEMS")
-        data = setdiff2d_bc(data, denyListArray)
+        # Removing duplicates
+        uniqueConvert = [tuple(row) for row in data]
+        data = np.unique(uniqueConvert, axis=0)
+        
+        # Remove entries that are already in main sheet and other sheets
+        data = setdiff2d_bc(data, mainFileArray)
+        data = setdiff2d_bc(data, mainData)
+        if len(denyListArray) > 0:
+            print("REMOVING DENY LIST ITEMS")
+            data = setdiff2d_bc(data, denyListArray)
 
-    # Parse through data
-    idx = -1
-    for team in data:
-        idx += 1
-        # If team is just "Traveler" it needs to know what kind of Traveler contextually so for now it must go to auto review.
-        if "Traveler" in team:
-            travelerReview = np.concatenate((travelerReview,[team]),axis=0)
+        # Parse through data
+        idx = -1
+        for team in data:
+            idx += 1
+            # If team is just "Traveler" it needs to know what kind of Traveler contextually so for now it must go to auto review.
+            if "Traveler" in team:
+                travelerReview = np.concatenate((travelerReview,[team]),axis=0)
+                data = np.delete(data, idx, axis=0)
+                idx -= 1
+                continue
+            # If either has character in fullSustain or a character in secondarySustain AND primarySustain, continue
+            if np.any(np.in1d(fullSustain, team)):
+                continue
+            if np.any(np.in1d(primarySustain, team)) and np.any(np.in1d(secondarySustain, team)):
+                continue
+            passFreezeCheck = True
+            hasCryo = False
+            hasHydro = False
+            # Check if freeze team
+            for character in team:
+                # Must have a hydro and cryo, and no pyro, geo, electro, dendro
+                for charData in characterData:
+                    if character == charData["shortName"]:
+                        if np.any(np.in1d(freezeTeamBans, charData["elements"])) or character == "Eula":
+                            passFreezeCheck = False
+                            break
+                        if "cryo" in charData["elements"]:
+                            hasCryo = True
+                        if "hydro" in charData["elements"]:
+                            hasHydro = True
+                if passFreezeCheck == False:
+                    break
+            if hasCryo == False or hasHydro == False:
+                passFreezeCheck = False
+            if passFreezeCheck:
+                continue
+            # Otherwise add to denyList array
+            denyListAppend = np.concatenate((denyListAppend,[team]),axis=0)
+            # Remove from main array
             data = np.delete(data, idx, axis=0)
             idx -= 1
-            continue
-        # If either has character in fullSustain or a character in secondarySustain AND primarySustain, continue
-        if np.any(np.in1d(fullSustain, team)):
-            continue
-        if np.any(np.in1d(primarySustain, team)) and np.any(np.in1d(secondarySustain, team)):
-            continue
-        passFreezeCheck = True
-        hasCryo = False
-        hasHydro = False
-        # Check if freeze team
-        for character in team:
-            # Must have a hydro and cryo, and no pyro, geo, electro, dendro
-            for charData in characterData:
-                if character == charData["shortName"]:
-                    if np.any(np.in1d(freezeTeamBans, charData["elements"])) or character == "Eula":
-                        passFreezeCheck = False
-                        break
-                    if "cryo" in charData["elements"]:
-                        hasCryo = True
-                    if "hydro" in charData["elements"]:
-                        hasHydro = True
-            if passFreezeCheck == False:
-                break
-        if hasCryo == False or hasHydro == False:
-            passFreezeCheck = False
-        if passFreezeCheck:
-            continue
-        # Otherwise add to denyList array
-        denyListAppend = np.concatenate((denyListAppend,[team]),axis=0)
-        # Remove from main array
-        data = np.delete(data, idx, axis=0)
-        idx -= 1
 
-    mainData = np.append(mainData, data, axis=0)
+        mainData = np.append(mainData, data, axis=0)
 
-    pf.close()
+        pf.close()
 
-    print("CLEANING UP FILES")
+        print("CLEANING UP FILES")
 
-    if os.path.exists(processFile[:-4] + "Filtered" + ".csv"):
-        os.remove(processFile[:-4] + "Filtered" + ".csv")
-    else:
-        print(processFile[:-4] + "Filtered" + ".csv" + "does not exist")
+        if os.path.exists(processFile[:-4] + "Filtered" + ".csv"):
+            os.remove(processFile[:-4] + "Filtered" + ".csv")
+        else:
+            print(processFile[:-4] + "Filtered" + ".csv" + "does not exist")
 
 
 # Remove bad Traveler teams
