20d8c1c5828ad49da3b8eb24f70e59af4bcdf346
Daniel Lee <daniellee0217@yahoo.com>
2022-12-18 18:23:23 -0500

    Fixed generation issues with 2nd half list

--- genshin_teams_filter_existing_teams 33_spire.py
+++ genshin_teams_filter_existing_teams 33_spire.py
@@ -9,6 +9,7 @@ secondarySustain = ["Xingqiu"]
 fullSustain = ["Barbara", "Bennett", "Diona", "Dori", "Jean", "Shinobu", "Noelle", "Qiqi", "Kokomi", "Sayu", "Zhongli"]
 freezeTeamBans = ["dendro", "electro", "geo", "pyro"]
 geoTeams = ["Albedo", "Zhongli", "TravelerGeo", "Ningguang"]
+banList = ["Diluc", "Aloy", "Amber", "Eula", "Razor", "Tighnari", "Yoimiya", "TravelerAnemo", "TravelerElectro"]
 
 def setdiff2d_bc(arr1, arr2):
     idx = (arr1[:, None] != arr2).any(-1).all(1)
@@ -34,97 +35,53 @@ mainFileArray = mainFileArray[1:]
 mainFileArray = np.sort(mainFileArray)
 fr.close()
 
-denyListArray = np.empty((0, 4), str)
-
-if os.stat(denyListFileName).st_size != 0:
-    print("LOADING DENY LIST FOR DENIED TEAMS")
-    dr = open(denyListFileName, 'r')
-    denyListArray = np.loadtxt(dr, delimiter=",", dtype=str, ndmin=2)
-    denyListArray = np.sort(denyListArray)
-    dr.close()
-
-mainData = np.empty((0, 4), str)
-
 # Moving entries that are lacking proper sustain (will not be stable for majority of players) to denyList automatically
 denyListAppend = np.empty((0, 4), str)
 # Moving entries that have ambiguous traveler and need to be manually reviewed to reviewTravelerTeams automatically
 travelerReview = np.empty((0, 4), str)
 
-for filename in os.listdir(directory):
-    processFile = os.path.join(directory, filename)
-    if os.stat(processFile).st_size != 0:
-        # Remove listings less than 4 rows
-        print("REMOVING TEAMS WITH LESS THAN 4 CHARACTERS")
-
-        with open(processFile, 'r') as infile, open(processFile[:-4] + "Filtered" + ".csv", 'w') as outfile:
-            csv_reader = csv.reader(infile)
-            csv_writer = csv.writer(outfile)
-            for line in csv_reader:
-                if all(line):
-                    csv_writer.writerow(line)
-
-        pf = open(processFile[:-4] + "Filtered" + ".csv", 'r')
-        data = np.loadtxt(pf, delimiter=",", dtype=str)
-
-        # Convert to shorthand
-        print("CONVERTING CHARACTER NAMES TO COMPATIBLE FORMAT")
+# Parse through data
+idx = -1
+for team in mainFileArray:
+    idx += 1
+    passGeoCheck = False
+    banned = False
+    # Check if freeze team
+    for character in team:
+        # Must have a hydro and cryo, and no pyro, geo, electro, dendro
         for charData in characterData:
-            if charData["fullName"] != "Traveler":
-                data[data == charData["fullName"]] = charData["shortName"]
-        data = np.sort(data)
-
-        # Removing duplicates
-        uniqueConvert = [tuple(row) for row in data]
-        data = np.unique(uniqueConvert, axis=0)
-        
-        # Remove entries that are already in main sheet and other sheets
-        if len(denyListArray) > 0:
-            print("REMOVING DENY LIST ITEMS")
-            data = setdiff2d_bc(data, denyListArray)
-
-        # Parse through data
-        idx = -1
-        for team in data:
-            idx += 1
-            passGeoCheck = False
-            # Check if freeze team
-            for character in team:
-                # Must have a hydro and cryo, and no pyro, geo, electro, dendro
-                for charData in characterData:
-                    if character == charData["shortName"]:
-                        if character in geoTeams:
-                            passGeoCheck = True
-                            break
-            if passGeoCheck == True:
-                # If team is just "Traveler" it needs to know what kind of Traveler contextually so for now it must go to auto review.
-                if "Traveler" in team:
-                    data = np.delete(data, idx, axis=0)
-                    idx -= 1
-                    continue
-                # If either has character in fullSustain or a character in secondarySustain AND primarySustain, continue
-                if np.any(np.in1d(fullSustain, team)):
-                    continue
-                if np.any(np.in1d(primarySustain, team)) and np.any(np.in1d(secondarySustain, team)):
-                    continue
-            # Remove from main array
-            data = np.delete(data, idx, axis=0)
+            if character == charData["shortName"]:
+                if character in banList:
+                    banned = True
+                    break
+                if character in geoTeams:
+                    passGeoCheck = True
+                    break
+    if banned == False and passGeoCheck == True:
+        # If team is just "Traveler" it needs to know what kind of Traveler contextually so for now it must go to auto review.
+        if "Traveler" in team:
+            mainFileArray = np.delete(mainFileArray, idx, axis=0)
             idx -= 1
+            continue
+        # If either has character in fullSustain or a character in secondarySustain AND primarySustain, continue
+        if np.any(np.in1d(fullSustain, team)):
+            continue
+        if np.any(np.in1d(primarySustain, team)) and np.any(np.in1d(secondarySustain, team)):
+            continue
+    # Remove from main array
+    mainFileArray = np.delete(mainFileArray, idx, axis=0)
+    idx -= 1
 
-        mainData = np.append(mainData, data, axis=0)
+print("CLEANING UP FILES")
 
-        pf.close()
-
-        print("CLEANING UP FILES")
-
-        if os.path.exists(processFile[:-4] + "Filtered" + ".csv"):
-            os.remove(processFile[:-4] + "Filtered" + ".csv")
-        else:
-            print(processFile[:-4] + "Filtered" + ".csv" + "does not exist")
+# Removing duplicates
+uniqueConvert = [tuple(row) for row in mainFileArray]
+mainFileArray = np.unique(uniqueConvert, axis=0)
 
 # Append data to aggregatedTeams
 
 print("SAVING OUTPUT CSV FILES")
 
-np.savetxt("./outputs/" + "aggregatedTeams33second.csv", mainData, fmt='%s', delimiter=',', newline='\n', header='', footer='', comments='# ', encoding=None)
+np.savetxt("./outputs/" + "aggregatedTeams33second.csv", mainFileArray, fmt='%s', delimiter=',', newline='\n', header='', footer='', comments='# ', encoding=None)
 
 print("COMPLETE")
