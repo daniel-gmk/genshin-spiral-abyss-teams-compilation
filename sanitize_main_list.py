d779ea8ae2eb32d2848b98cdb4755f2f54f9597e
Daniel Lee <daniellee0217@yahoo.com>
2023-01-17 17:48:36 -0500

    Adding new characters and filtering/sorting list

--- sanitize_main_list.py
+++ sanitize_main_list.py
@@ -8,8 +8,6 @@ primarySustain = ["Hu Tao", "Beidou", "Thoma", "Layla", "Xinyan"]
 secondarySustain = ["Xingqiu"]
 fullSustain = ["Barbara", "Bennett", "Diona", "Dori", "Jean", "Shinobu", "Noelle", "Qiqi", "Kokomi", "Sayu", "Zhongli"]
 freezeTeamBans = ["dendro", "electro", "geo", "pyro"]
-geoTeams = ["Albedo", "Zhongli", "TravelerGeo", "Ningguang"]
-banList = ["Diluc", "Aloy", "Amber", "Eula", "Razor", "Tighnari", "Yoimiya", "TravelerAnemo", "TravelerElectro"]
 
 def setdiff2d_bc(arr1, arr2):
     idx = (arr1[:, None] != arr2).any(-1).all(1)
@@ -40,38 +38,6 @@ denyListAppend = np.empty((0, 4), str)
 # Moving entries that have ambiguous traveler and need to be manually reviewed to reviewTravelerTeams automatically
 travelerReview = np.empty((0, 4), str)
 
-# Parse through data
-idx = -1
-for team in mainFileArray:
-    idx += 1
-    passGeoCheck = False
-    banned = False
-    # Check if freeze team
-    for character in team:
-        # Must have a hydro and cryo, and no pyro, geo, electro, dendro
-        for charData in characterData:
-            if character == charData["shortName"]:
-                if character in banList:
-                    banned = True
-                    break
-                if character in geoTeams:
-                    passGeoCheck = True
-                    break
-    if banned == False and passGeoCheck == True:
-        # If team is just "Traveler" it needs to know what kind of Traveler contextually so for now it must go to auto review.
-        if "Traveler" in team:
-            mainFileArray = np.delete(mainFileArray, idx, axis=0)
-            idx -= 1
-            continue
-        # If either has character in fullSustain or a character in secondarySustain AND primarySustain, continue
-        if np.any(np.in1d(fullSustain, team)):
-            continue
-        if np.any(np.in1d(primarySustain, team)) and np.any(np.in1d(secondarySustain, team)):
-            continue
-    # Remove from main array
-    mainFileArray = np.delete(mainFileArray, idx, axis=0)
-    idx -= 1
-
 print("CLEANING UP FILES")
 
 # Removing duplicates
@@ -82,6 +48,6 @@ mainFileArray = np.unique(uniqueConvert, axis=0)
 
 print("SAVING OUTPUT CSV FILES")
 
-np.savetxt("./outputs/" + "aggregatedTeams33second.csv", mainFileArray, fmt='%s', delimiter=',', newline='\n', header='', footer='', comments='# ', encoding=None)
+np.savetxt("genshinTeamsNamedFiltered.csv", mainFileArray, fmt='%s', delimiter=',', newline='\n', header='', footer='', comments='# ', encoding=None)
 
 print("COMPLETE")
